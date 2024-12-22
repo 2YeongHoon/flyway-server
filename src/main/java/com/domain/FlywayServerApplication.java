4223c296fa4bed553eb5440afff91d67a895972a
yeonghoon.lee <djaakclsrn12@naver.com>
2024-12-22 23:28:27 +0900

    feat: config scan 위치 지정

--- src/main/java/com/domain/FlywayServerApplication.java
+++ src/main/java/com/domain/FlywayServerApplication.java
@@ -2,8 +2,10 @@ package com.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(value = "com.domain")
 public class FlywayServerApplication {
 
     public static void main(String[] args) {
